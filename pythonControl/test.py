37ae0f5fa0f0682b164bade09ae5ebf08e6eeae6
MarcoAAG <marco-antonioa@outlook.com>
2020-10-15 18:02:47 -0500

    Added Control design

--- pythonControl/test.py
+++ pythonControl/test.py
@@ -4,21 +4,23 @@ import control
 import math
 import numpy as np
 
+def planta(num, den):
+    G = control.tf(num,den)
+    Gc = control.feedback(G)
+    return G
 
-
-
-def PIController(Ts, Mp):
-    #! Planta
-    numG = [10]
-    denG = [1, 0]
-    G = control.tf(numG,denG)
+def PIController(Ts, Mp, G):
+    # #! Planta
+    # numG = [10]
+    # denG = [1, 0]
+    # G = control.tf(numG,denG)
     
     #!Parametro de diseo para control PI
     # Ts = 1.3    #* Tiempo de asentamiento
     # Mp = 2.0      #* Porcentaje de sobre impulso
     # Tol = 5     #* Tolerancia de error %
     z = math.sqrt((np.log(Mp/100.0))**2 / ((math.pi ** 2) + (np.log(Mp/100.0))**2))     #* Calcular Factor de amortiguamiento
-    Wn = 5.0 / (z*Ts)       #*Calcular frecuencia natural
+    Wn = 6.0 / (z*Ts)       #*Calcular frecuencia natural
     
     #! Calcular Polos Deseados
     numD = [Wn**2]
@@ -26,15 +28,17 @@ def PIController(Ts, Mp):
     Gd = control.tf(numD, denD)
     
     #! Calcular Ganancias
-    # Kc = ((2.0 * z * Wn) - 1.0)/10.0
-    # Ti = Kc/(Wn**2) * 10.0
-    Kc = (2.0 * z * Wn) / 10.0
-    Ti = 10.0*Kc / (Wn**2)
+    Kc = ((2.0 * z * Wn) - 1.0)/numG[0]
+    Ti = Kc/(Wn**2) * numG[0]
+    # Kc = (2.0 * z * Wn) / numG[0]
+    # Ti = numG[0]*Kc / (Wn**2)
     print("--------------------------------------------------")
     print("Ts = ", Ts)
+    print(Gd)
+    print(control.pole(Gd))
     print("Ganancia Kp = ", Kc) 
     print("Ganancia Ki = ", Ti)
-    
+
     #! Obtener Funcion de transferencia de control PI
     numC = [Kc*Ti, Kc]
     denC = [Ti, 0.0]
@@ -48,37 +52,27 @@ def PIController(Ts, Mp):
 
 if __name__ == "__main__":
 
-    TS = [0.1,0.5,1.0,1.5,2.0,3.0]
-    # MP = [0.1, 0.1]
-    for i in range(0,len(TS)):
-        C = PIController(TS[i], 0.10)
-        T, y = control.step_response(C)
-        plt.plot(T, y)
+    numG = [125]
+    denG = [1, 0]
+    G = planta(numG,denG)
 
-    # C1 = PIController(1.3, 0.1)
-    # T, y = control.step_response(C1)
-    # plt.plot(T, y)
+    # T, y = control.step_response(G)
+    # plt.plot(T, y, linewidth=2.0)
 
-    # C2 = PIController(2.0, 2.0)
-    # T, y = control.step_response(C2)
-    # plt.plot(T, y)
+    # TS = [0.2,0.5,1.0,1.5,2.0,3.0]
+    # for i in range(0,len(TS)):
+        # C = PIController(TS[i], 0.10, G)
+        # T, y = control.step_response(C)
+        # tmp = 'Ts = ' + str(TS[i])
+        # plt.plot(T, y,label = tmp, linewidth=2.0)
 
-    plt.xlabel('Tiempo [s]')
-    plt.ylabel('Amplitud')
-    plt.title('Respuesta escalon de Pitch')
+    C = PIController(0.3,0.1,G)
+    T, y = control.step_response(C)
+    plt.plot(T, y, linewidth = 2.0)
+
+    # plt.xlabel('Tiempo [s]')
+    # plt.ylabel('Amplitud')
+    # plt.title('Respuesta escalon de Pitch')
     plt.grid()
+    # plt.legend()
     plt.show()
-
-
-
-
-
-# lti = signal.lti([14.8], [1.0, 6, 14.8])
-# print(control.dcgain(lti))
-# t, y = signal.step(lti)
-# plt.plot(t, y)
-# plt.xlabel('Tiempo [s]')
-# plt.ylabel('Amplitud')
-# plt.title('Respuesta escalon de Pitch')
-# plt.grid()
-# plt.show()
